eb725217d6417f5a1a5d4b539ea506ca3cf877ef
Vardges <vardgeshyan@gmail.com>
2022-01-31 00:21:13 +0400

    eeee

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
-echo "Build project"
-npm run build
+echo "Pre-Build Steps:"
+echo "authenticating with AWS ECR..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 112356928409.dkr.ecr.us-east-1.amazonaws.com
 
-echo "Use react-cfb47 id for deployment"
-firebase use react-cfb47
+echo "Build Steps:"
+echo "building image..."
+docker build --platform=linux/amd64 -t 112356928409.dkr.ecr.us-east-1.amazonaws.com/react-docker-aws:latest .
 
-echo "Deploy build folder"
-firebase deploy --only hosting
+echo "Post-Build Steps:"
+echo "pushing image to AWS ECR..."
+docker push 112356928409.dkr.ecr.us-east-1.amazonaws.com/react-docker-aws:latest
+
+aws ecs update-service --desired-count 0 --force-new-deployment --service react-sv-2 --cluster react-cluster-2
+
+aws ecs update-service --desired-count 1 --force-new-deployment --service react-sv-2 --cluster react-cluster-2
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1>Heloo firebase barevvvvv</h1>
+      <h1>Heloo eeeee</h1>
     </div>
   );
 };
